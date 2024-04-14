fd83914f9db5f872dcb9205a8d22a233b386b51e
Valencia <121145298+ValenValine@users.noreply.github.com>
2024-04-14 20:57:52 +0900

    Add bank.py
    https://cs50.harvard.edu/python/2022/psets/1/bank/

---
+++ bank.py
@@ -0,0 +1,10 @@
+def main():
+    greeting = input("Greeting:")
+    if "Hello" in greeting:
+        print("$0")
+    elif "H" in greeting:
+        print("$20")
+    else:
+        print("$100")
+
+main()
